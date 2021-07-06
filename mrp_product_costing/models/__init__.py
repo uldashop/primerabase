fc9f94dac7ed0a483aaeaefeb67fdd5958caad37
Jose Garcia <jgarcia@opa-consutlting.com>
2021-07-06 12:49:36 -0500

    06072021 noMRP

--- mrp_product_costing/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import mrp_product_costing_parameters
-from . import mrp_bom
-from . import mrp_production
-from . import mrp_actual_posting
-from . import mrp_economical_closure
-#from . import report
