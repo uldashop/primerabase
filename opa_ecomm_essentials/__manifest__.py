87355452ccea6a1f0d23cda23e378ec84612df08
Jose Garcia <jgarcia@opa-consutlting.com>
2021-07-06 12:15:06 -0500

    06072021 opaecomm

--- opa_ecomm_essentials/__manifest__.py
+++ opa_ecomm_essentials/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website', 'website_sale', 'theme_clarico_vega', 'website_sale_require_login'],
+    'depends': ['website', 'website_sale', 'website_sale_require_login'],
 
     # always loaded
     'data': [
