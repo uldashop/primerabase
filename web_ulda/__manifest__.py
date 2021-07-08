659444e2d3b02e783653db1da35e27e433473136
Alexander <alexander.morenoc@outlook.com>
2021-07-08 10:14:07 -0500

    revision 08072021

---
+++ web_ulda/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web Ulda",
+
+    'summary': """Web de Ulda""",
+
+    'description': """
+        Web de Ulda
+    """,
+
+    'author': "Opa Consulting",
+    'website': "https://www.opa-consulting.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','website', 'theme_clarico_vega'],
+
+    # always loaded
+    'data': [
+        'templates/assets.xml',
+        'templates/header.xml',
+        'templates/footer.xml',
+        'templates/dev.xml',
+        # 'templates/web_pages_snnipets.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ web_ulda/static/src/js/header.js
@@ -0,0 +1,25 @@
+const $header = document.querySelector('.te_header_style_2_main')
+
+const $header_icon_wish = $header.querySelector('.te_wish_icon_head a')
+const $header_icon_cart = $header.querySelector('.te_cart_icon_head a')
+const $header_icon_user = $header.querySelector('.te_user_account_name')
+
+const $imagen_wish= document.createElement('img')
+const $imagen_cart= document.createElement('img')
+const $imagen_user= document.createElement('img')
+
+$imagen_wish.setAttribute('src', '/web_ulda/static/img/site/header/heart.png')
+$imagen_cart.setAttribute('src', '/web_ulda/static/img/site/header/shopping-cart.png')
+$imagen_user.setAttribute('src', '/web_ulda/static/img/site/header/user-circle.png')
+
+$header_icon_wish.appendChild($imagen_wish)
+$header_icon_cart.appendChild($imagen_cart)
+$header_icon_user.appendChild($imagen_user)
+
+const icon_i_wish=$header_icon_wish.querySelector('i')
+const icon_i_cart=$header_icon_cart.querySelector('i')
+const icon_i_user=$header_icon_user.querySelector('i')
+
+icon_i_wish.setAttribute('style', 'display:none!important;')
+icon_i_cart.setAttribute('style', 'display:none!important;')
+icon_i_user.setAttribute('style', 'display:none!important;')
