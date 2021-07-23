6eff9e51a0c4a53f1b3120f581c398fbe7729cc2
Jose Garcia <jgarcia@opa-consutlting.com>
2021-07-23 14:44:43 -0500

    sans clarico

--- theme_clarico_vega/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import theme_clarico_vega
--- web_ulda/__manifest__.py
+++ web_ulda/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website', 'theme_clarico_vega'],
+    'depends': ['base','website'],
 
     # always loaded
     'data': [
